8d3eb9eb568118ca637605159e594cb35b38b21f
abhay15032008 <abhay.25b01011377@abes.ac.in>
2026-06-14 22:43:25 +0530

    dispalyjs

---
+++ display.js
@@ -0,0 +1,20 @@
+SWITCH STATTEMENT  
+  let day='monday';
+  switch(day){
+   case'monday':
+   console.log('shiv ji ki pooja karo');
+   break;
+   case'tuesday':
+   console.log('hanuman ji ki pooja karo');
+   break;
+   case'wednesday':
+   console.log('brahma ji ki pooja karo');
+   break;
+   case'thursday':
+   console.log('sai ji ki pooja karo');
+   break;
+   default:
+      console.log('tum paapi ho saale rakshass !!');
+  } 
+
+alert('something is wrong');
