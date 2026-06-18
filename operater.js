45d8d2b2052d53f5221e2ca1f5020cdf5335b90f
abhay15032008 <abhay.25b01011377@abes.ac.in>
2026-06-18 22:03:29 +0530

    operater

---
+++ operater.js
@@ -0,0 +1,29 @@
+//unary operator
+
+let a=10;
+let b=6;
+console.log(a++);
+console.log(++a);
+
+//Assignment operators
+
+let c=10;
+let d=5;
+console.log(d);//5
+console.log(c);//15
+console.log(c-=d);//10
+console.log(c*=d);//50
+console.log(c/=d);//10
+console.log(c%=d);//0
+
+//comparison operator
+
+let age=22;
+age=18;
+console.log(age>18);
+console.log(age>=18);
+console.log(age<18);
+console.log(age<=18);
+console.log(age==18);
+console.log(age!=18);
+
