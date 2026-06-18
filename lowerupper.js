a875e416985fd74d924145fe154158a143c9f4e8
abhay15032008 <abhay.25b01011377@abes.ac.in>
2026-06-18 21:55:57 +0530

    lowerupper

---
+++ lowerupper.js
@@ -0,0 +1,11 @@
+let msg= '      hello    ';
+let password=prompt('enter your password');
+console.log(password.trim());
+
+
+
+// to lower/uppercase method
+
+ let day="mondAY";
+ console.log(day.toUpperCase())
+ console.log(day.toLowerCase());
